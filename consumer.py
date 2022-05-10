1a789ba23950bd03c9e465464235f5e96f2d4305
defilippomattia <defilippomattia@gmail.com>
2022-05-10 23:39:48 +0200

    a

--- consumer.py
+++ consumer.py
@@ -1,3 +1,4 @@
+from re import M
 from time import sleep
 import streamlit as st
 import pandas as pd
@@ -7,20 +8,32 @@ import json
 from kafka import KafkaConsumer
 my_map = st.map()
 
-my_nd_arr = np.array([[1, 2]])
+#my_nd_arr = np.array([[1, 2]])
+# my_nd_arr = np.array([
+#         [5.98,32.5],
+#         ])
+my_nd_arr = np.array([
+        [45.08127861241874, 18.160400390625]])
 def getdf(msg):
     global my_nd_arr
     print(msg)
     print(msg[0])
     print(msg[1])
     print("---")
+    novi = np.array([[float(msg[1]),float(msg[0])]])
     # my_nd_arr = np.array([
-    #     [float(msg[1]),float(msg[0])],
-    #     [float(msg[0]),float(msg[1])]
+    #     [float(msg[0]),float(msg[1])],
     #     ])
+    # my_nd_arr = 
+    my_nd_arr = np.append(my_nd_arr,novi,axis=0)
     #my_nd_arr.append(my_nd_arr,[float(msg[1]),float(msg[0])])
-    my_nd_arr = np.append(my_nd_arr,[float(msg[1]),float(msg[0])])
+    #my_nd_arr = np.append(my_nd_arr,[float(msg[1]),float(msg[0])])
     print(my_nd_arr)
+    # df = pd.DataFrame(
+    #     my_nd_arr,
+    #     columns=['lat', 'lon']
+    # )
+        
     df = pd.DataFrame(
         my_nd_arr,
         columns=['lat', 'lon']
